2f45eee613e36abfae7d59dc2c5da0e897fb2a09
Stuflo19 <szflorence@dundee.ac.uk>
2021-12-19 04:07:06 +0000

    Binary Tree working

--- 18/Binary_Tree.py
+++ 18/Binary_Tree.py
@@ -1,17 +1,26 @@
-import Node
+import Node as n
 
 class Binary_Tree:
+    def __init__(self):
+        self.root = None
+
     def create_tree(self, value):
         if type(value) == int:
-            return Node(value, None, None)
+            return n.Node(value, None, None)
         
         left_node = self.create_tree(value[0])
         right_node = self.create_tree(value[1])
 
-        parent = Node(None, left_node, right_node)
-        left_node.parent = parent
-        right_node.parent = parent
-    
-    def add_snail_number(value):
-        l = value[0]
-        r = value[1]
+        self.root = n.Node(None, left_node, right_node)
+        left_node.parent = self.root
+        right_node.parent = self.root
+
+        return self.root
+
+    def add_new_tree(self, old, new):
+        new_root = n.Node(None, old, new)
+
+        old.parent = new_root
+        new.parent = new_root
+
+        self.root = new_root
--- 18/solution.py
+++ 18/solution.py
@@ -1,39 +1,42 @@
+import Binary_Tree as bt
+#GLOBAL VARIABLES
+global tree
+
+#Function that reads in lines from the file as a list and strips them of the \n char
 def open_file(file_name):
     with open(file_name) as input:
-        lines = [list(line.strip()) for line in (input.readlines())]
+        lines = input.readlines()
         print(lines)
     return lines
-
-def line_to_array(line):
-    print("\n")
-    print(line)
-    quit()
 
 def split(curr_sum):
     new_sum = []
 
 def explode(curr_sum):
-    new_sum = []
-
-def add(curr_sum):
-    new_sum = []
+    new = None
 
 def find_sum(lines):
-    curr_sum = []
-    magnitude = 0
+    tree = None
 
     for line in lines:
-        print(line)
-        quit()
+        snail_num = eval(line)
+        print(snail_num)
 
-        curr_sum.append(line)
+        #checks if any tree exists
+        if tree is None:
+            tree = bt.Binary_Tree()
+            tree.create_tree(snail_num) #creates the brand new tree
+            continue
+    
+        new_num = bt.Binary_Tree()
+        new_num.create_tree(snail_num) #creates new snail number tree
+        tree.add_new_tree(tree.root, new_num.root) #adds new snail number tree the the right and old tree to the left
 
-    return magnitude
+        del new_num #clear the memory allocated to new_num
+        
 
 def main():
     lines = open_file('18/input.txt')
-
-    line_to_array(lines)
 
     find_sum(lines)
 
