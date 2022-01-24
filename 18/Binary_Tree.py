9c9bf4eb59869e3246888188ed6f5dd157868031
Stuflo19 <szflorence@dundee.ac.uk>
2022-01-24 22:17:46 +0000

    Uncomplete changes

--- 18/Binary_Tree.py
+++ 18/Binary_Tree.py
@@ -6,19 +6,19 @@ class Binary_Tree:
 
     def create_tree(self, value):
         if type(value) == int:
-            return n.Node(value, None, None)
+            return n.Node(None, None, value)
         
         left_node = self.create_tree(value[0])
         right_node = self.create_tree(value[1])
 
-        self.root = n.Node(None, left_node, right_node)
+        self.root = n.Node(left_node, right_node, None)
         left_node.parent = self.root
         right_node.parent = self.root
 
         return self.root
 
     def add_new_tree(self, old, new):
-        new_root = n.Node(None, old, new)
+        new_root = n.Node(old, new, None)
 
         old.parent = new_root
         new.parent = new_root
--- 18/solution.py
+++ 18/solution.py
@@ -1,6 +1,4 @@
 import Binary_Tree as bt
-#GLOBAL VARIABLES
-global tree
 
 #Function that reads in lines from the file as a list and strips them of the \n char
 def open_file(file_name):
@@ -12,14 +10,53 @@ def open_file(file_name):
 def split(curr_sum):
     new_sum = []
 
-def explode(curr_sum):
+def explode_left(val):
+    root = tree.root
+    to_find = tree.root
+
+    while root.left is None or root.left == to_find:
+        to_find = root
+        root = root.parent
+
+        if root is None: return
+
+    root = root.l
+
+    while root.r is not None:
+        root = root.r
+    
+    root.val += val
+
+def explode_right(val):
+    root = tree.root
+    to_find = root.copy()
+
+
+    while root.right is None or root.right == to_find:
+        to_find = root
+        root = root.parent
+
+        if root is None: return
+
+    root = root.r
+
+    while root.l is not None:
+        root = root.l
+
+    root.val += 1
+
+def explode(node, depth):
     new = None
+    
+    if depth == 4 and node.val is None:
 
 def find_sum(lines):
+    global tree
     tree = None
 
+    #for every line of the file add it to the binary tree
     for line in lines:
-        snail_num = eval(line)
+        snail_num = eval(line) #inputs the line as an array
         print(snail_num)
 
         #checks if any tree exists
@@ -32,8 +69,7 @@ def find_sum(lines):
         new_num.create_tree(snail_num) #creates new snail number tree
         tree.add_new_tree(tree.root, new_num.root) #adds new snail number tree the the right and old tree to the left
 
-        del new_num #clear the memory allocated to new_num
-        
+        del new_num #clear the memory allocated to new_num     
 
 def main():
     lines = open_file('18/input.txt')
